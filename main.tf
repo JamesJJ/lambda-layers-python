43a672793d9b07db52f076eefb7d113280831d6c
JamesJJ <jj@fcg.fyi>
2023-07-29 11:18:52 +0800

    Update for python3.11, boto3==1.28.15, botocore==1.31.15

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_layer_version" "boto3_botocore_requests" {
   filename                 = "${local.lambda_layer_dir}/package.zip"
   source_code_hash         = filebase64sha256("${local.lambda_layer_dir}/package.zip")
   layer_name               = "boto3_botocore_requests"
-  compatible_runtimes      = ["python3.10", "python3.9"]
+  compatible_runtimes      = ["python3.11", "python3.10", "python3.9"]
   compatible_architectures = ["arm64", "x86_64"]
 }
 
