b7d2cccc90c6ffc0164c684f5edba94e3f1387eb
JamesJJ <jj@fcg.fyi>
2023-12-26 13:10:59 +0800

    Use containerised build environment if available

--- layers/boto3_botocore_requests/make_package.sh
+++ layers/boto3_botocore_requests/make_package.sh
@@ -4,11 +4,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-PIP="$(which pip3)" || PIP="pip"
+HERE="$(dirname "$( realpath "$0" )" )"
+
+if finch -v 2>/dev/null ; then
+    finch vm start 2>/dev/null || true
+    finch run -it --rm -v "${HERE}:/workdir" public.ecr.aws/sam/build-python3.11 bash -c "cd /workdir && bash $(basename $0)"
+    exit $?
+fi
+
+uname -a
+
+PIP="$(which pip3 2>/dev/null)" || PIP="pip"
 
 TMPDIR="$(mktemp -d -t lambda_package_layer.XXXXXXXXXX)" || exit 1
-
-HERE="$(dirname "$( realpath "$0" )" )"
 
 $PIP install --platform manylinux2014_aarch64 --platform manylinux2014_x86_64 --implementation cp --python-version 3.11 --only-binary=:all: --target "${TMPDIR}/python" -r "${HERE}/requirements.txt"
 
@@ -16,7 +24,9 @@ cd "${TMPDIR}"
 
 rm -f "${HERE}/package.zip"
 
-zip -r "${HERE}/package.zip" .
+zip -q -r "${HERE}/package.zip" . -x '*/__pycache__/*'
+
+ls -l "${HERE}/package.zip"
 
 cd /
 
